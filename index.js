5bb4b20b3869748af7ba0b7a8be58a991827b9e7
xxapp <552062803@qq.com>
2017-12-14 01:02:28 +0800

    使用代码生成表格

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
 
 const dragDrop$ = mousedown$.switchMap(() => mousemove$.takeUntil(mouseup$));
 
+renderTable(table);
+
 mousedown$
     .filter(e => e.target.nodeName === 'TD')
     .subscribe(e => {
@@ -18,3 +20,23 @@ mousedown$
                 e.target.className = 'selected';
             });
     });
+
+function renderTable(table) {
+    const column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    const row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const frag = document.createDocumentFragment();
+    
+    row.forEach(i => {
+        const tr = document.createElement('tr');
+        tr.id = 'row-' + i;
+        column.forEach(j => {
+            const td = document.createElement('td');
+            td.id = `cell-${i}-${j}`;
+            td.setAttribute('data-row', i);
+            td.setAttribute('data-column', j);
+            tr.appendChild(td);
+        });
+        frag.appendChild(tr);
+    });
+    table.appendChild(frag);
+}
