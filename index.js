74d2d4a5228a38ce391fe0fcdd0f80b493825db6
mengxiangxiang <mengxiangxiang@souche.com>
2017-12-14 22:11:03 +0800

    实现拖拽选择单元格功能

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Rx = require('rxjs');
 
 const table = document.getElementById('main_table');
-const mousedown$ = Rx.Observable.fromEvent(table, 'mousedown');
-const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove');
+const mousedown$ = Rx.Observable.fromEvent(table, 'mousedown').filter(e => e.target.nodeName === 'TD');
+const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove').filter(e => e.target.nodeName === 'TD');
 const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
 
 const dragDrop$ = mousedown$.switchMap(() => mousemove$.takeUntil(mouseup$));
@@ -10,19 +10,35 @@ const dragDrop$ = mousedown$.switchMap(() => mousemove$.takeUntil(mouseup$));
 renderTable(table);
 
 mousedown$
-    .filter(e => e.target.nodeName === 'TD')
     .subscribe(e => {
         table.querySelectorAll('td').forEach(cell => {cell.className = '';});
-        e.target.className = 'selected';
         dragDrop$
-            .filter(e => e.target.nodeName === 'TD')
-            .subscribe(e => {
-                e.target.className = 'selected';
+            .scan((acc, e) => {
+                const pos = getPosition(e.target);
+                if (!acc) {
+                    return { startRow: pos.row, startColumn: pos.column, endRow: pos.row, endColumn: pos.column };
+                } else {
+                    return Object.assign(acc, { endRow: pos.row, endColumn: pos.column });
+                }
+            }, null)
+            .subscribe(range => {
+                const startRow = Math.min(range.startRow, range.endRow);
+                const startColumn = Math.min(range.startColumn, range.endColumn);
+                const endRow = Math.max(range.startRow, range.endRow);
+                const endColumn = Math.max(range.startColumn, range.endColumn);
+                table.querySelectorAll('td').forEach(cell => {cell.className = '';});
+                for (let i = startRow; i < endRow + 1; i++) {
+                    for (let j = startColumn; j < endColumn + 1; j++) {
+                        document.getElementById(`cell-${i}-${j}`).className = 'selected';
+                    }
+                }
             });
     });
 
+
+
 function renderTable(table) {
-    const column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    const column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];;
     const row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const frag = document.createDocumentFragment();
     
@@ -39,4 +55,11 @@ function renderTable(table) {
         frag.appendChild(tr);
     });
     table.appendChild(frag);
+}
+
+function getPosition(el) {
+    return {
+        row: parseInt(el.getAttribute('data-row')),
+        column: parseInt(el.getAttribute('data-column')),
+    };
 }
