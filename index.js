c59be1770088ceaa8933e24a457bd1d1ad9a83fe
mengxiangxiang <mengxiangxiang@souche.com>
2017-12-24 22:25:39 +0800

    重构拖拽逻辑，实现表格编辑功能

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 const Rx = require('rxjs');
 
 const table = document.getElementById('main_table');
+const cellInput = document.getElementById('cell_input');
 const mousedown$ = Rx.Observable.fromEvent(table, 'mousedown').filter(e => e.target.nodeName === 'TD');
 const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove').filter(e => e.target.nodeName === 'TD');
 const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
-
-const dragDrop$ = mousedown$.switchMap(() => mousemove$.takeUntil(mouseup$));
+const click$ = Rx.Observable.fromEvent(table, 'click').filter(e => e.target.nodeName === 'TD');
 
 renderTable(table);
 
-mousedown$
-    .subscribe(e => {
-        dragDrop$
-            .scan((acc, e) => {
-                const pos = getPosition(e.target);
+const dragDrop$ = mousedown$
+    .map(e => {
+        return Rx.Observable.of(e).switchMap(() => mousemove$.takeUntil(mouseup$))
+            .startWith(e)
+            .map(e => getPosition(e.target))
+            .distinctUntilChanged((p, q) => isPositionEqual(p, q))
+            .scan((acc, pos) => {
                 if (!acc) {
                     return { startRow: pos.row, startColumn: pos.column, endRow: pos.row, endColumn: pos.column };
                 } else {
                     return Object.assign(acc, { endRow: pos.row, endColumn: pos.column });
                 }
-            }, null)
-            .subscribe(range => {
-                const startRow = Math.min(range.startRow, range.endRow);
-                const startColumn = Math.min(range.startColumn, range.endColumn);
-                const endRow = Math.max(range.startRow, range.endRow);
-                const endColumn = Math.max(range.startColumn, range.endColumn);
-                const { top, left } = document.getElementById(`cell-${startRow}-${startColumn}`).getBoundingClientRect();
-                const { bottom, right } = document.getElementById(`cell-${endRow}-${endColumn}`).getBoundingClientRect();
-                const selectionEl = document.getElementById('selection');
-                selectionEl.style.top = `${top - 2}px`;
-                selectionEl.style.left = `${left - 2}px`;
-                selectionEl.style.height = `${bottom - top + 3}px`;
-                selectionEl.style.width = `${right - left + 3}px`;
-            });
-    });
+            }, null);
+    })
+    .mergeAll();
 
+const doubleClick$ = click$
+    .bufferCount(2, 1)
+    .filter(([e1, e2]) => e1.target.id === e2.target.id)
+    .map(([e]) => e);
 
+dragDrop$.subscribe(renderSelection);
+doubleClick$.subscribe((e) => {
+    e.target.appendChild(cellInput);
+    cellInput.style.removeProperty('display');
+    cellInput.setAttribute('value', '');
+    cellInput.style.outline = 'none';
+    cellInput.focus();
+});
+
+function renderSelection(range) {
+    const startRow = Math.min(range.startRow, range.endRow);
+    const startColumn = Math.min(range.startColumn, range.endColumn);
+    const endRow = Math.max(range.startRow, range.endRow);
+    const endColumn = Math.max(range.startColumn, range.endColumn);
+    const { top, left } = document.getElementById(`cell-${startRow}-${startColumn}`).getBoundingClientRect();
+    const { bottom, right } = document.getElementById(`cell-${endRow}-${endColumn}`).getBoundingClientRect();
+    const { scrollTop, scrollLeft } = document.documentElement;
+    const selectionEl = document.getElementById('selection');
+    selectionEl.style.top = `${top + scrollTop - 2}px`;
+    selectionEl.style.left = `${left + scrollLeft - 2}px`;
+    selectionEl.style.height = `${bottom - top + 3}px`;
+    selectionEl.style.width = `${right - left + 3}px`;
+}
 
 function renderTable(table) {
-    const column = Array(10).fill(1).map((n, i) => i + 1);
-    const row = Array(20).fill(1).map((n, i) => i + 1);
+    const column = Array(10).fill(false).map((n, i) => i + 1);
+    const row = Array(20).fill(false).map((n, i) => i + 1);
     const frag = document.createDocumentFragment();
     
     row.forEach(i => {
@@ -54,6 +71,7 @@ function renderTable(table) {
         });
         frag.appendChild(tr);
     });
+    table.innerHTML = '';
     table.appendChild(frag);
 }
 
@@ -62,4 +80,8 @@ function getPosition(el) {
         row: parseInt(el.getAttribute('data-row')),
         column: parseInt(el.getAttribute('data-column')),
     };
+}
+
+function isPositionEqual(pos1, pos2) {
+    return pos1.row === pos2.row && pos1.column === pos2.column;
 }
